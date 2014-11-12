a55170712453557bf70213c89dddbc4c8043ce4a
Christopher Durr <christopherdurr1@gmail.com>
2014-11-11 20:22:48 -0800

    Update lisp.js

--- lisp.js
+++ lisp.js
@@ -1,1 +1,54 @@
 //use nodejs on this file
+/*
+This is a simple lisp-like interpreter implemented in nodejs
+
+Created by Christopher Durr 2014
+*/
+
+var input;
+var operator; 
+var total;
+var index;
+
+input = process.argv;
+operator = input[2];
+if ((operator == "+") || (operator == "add"))
+{
+	total = 0;
+	for (index = 3; index < input.length; index++)
+	{
+		total += Number(input[index])
+	}
+	console.log(total);
+}
+else if ((operator == "-") ||(operator == "subtract"))
+{
+	total = input[3];
+	for (index = 4; index < input.length; index++) 
+	{
+		total -= Number(input[index]);
+	}
+	console.log(total);
+}
+else if (operator == "multiply")
+{
+	total = 1;
+	for (index = 3; index < input.length; index++)
+	{
+		total *= Number(input[index]);
+	}
+	console.log(total);
+}
+else if ((operator == "/") || (operator == "divide"))
+{
+	total = input[3]
+	for(index = 4; index < input.length; index++)
+	{
+		total /= Number(input[index])
+	}
+	console.log(total);
+}
+else
+{
+	console.log("Didn't understand operator!");
+}
